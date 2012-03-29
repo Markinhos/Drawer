b0bc3ca9802a6d1ccfb60bfc2b6777c72623a226
Marcos <marcos.arranz.mateo@gmail.com>
2012-03-29 14:17:08 +0200

    first model

--- drawerApp/models.py
+++ drawerApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField('Date created')
