fa3d72f6a84a551b065ae843b5e1c04abbcb4593
Marcos Arranz <marcos.arranz.mateo@gmail.com>
2012-06-27 12:10:08 +0200

    remove sites from installed apps

--- drawapp/drawerApp/views.py
+++ drawapp/drawerApp/views.py
@@ -6,5 +6,4 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def index(request):
     project_collection = Project.objects.all()
-    output = ', '.join([p.title for p in project_collection])
     return render_to_response('home.html', { 'project_collection' : project_collection}, context_instance = RequestContext(request))
