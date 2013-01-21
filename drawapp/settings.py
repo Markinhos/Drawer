e9dca927a1a2a77b814b64333b6d6b1f048e81b9
Marcos Arranz <marcos.arranz.mateo@gmail.com>
2013-01-21 20:21:19 +0100

    Connecting to memcached

--- drawapp/settings.py
+++ drawapp/settings.py
@@ -254,6 +254,29 @@ DROPBOX_AUTH_SECRET = 'xciu5lbxmiiboju'
 
 DROPBOX_ACCESS_TYPE = 'app_folder'
 
+def get_cache():
+  import os
+  try:
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS']
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'LOCATION': os.environ['MEMCACHIER_SERVERS'],
+        'TIMEOUT': 500,
+        'BINARY': True,
+      }
+    }
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
+
 try:
     from local_settings import *
 except ImportError:
